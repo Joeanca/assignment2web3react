a93fe78f858b3d28b1ba1794b8fb473aaeb309d4
Brandon Cochrane <bcoch346@mtroyal.ca>
2018-04-09 02:35:08 -0600

    changed index

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+//import './index.scss';
+import App from './src/App';
+import registerServiceWorker from './src/registerServiceWorker';
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
+registerServiceWorker();
